8a60159c7b4a92eb1a3cc25d55f26bb1218ee788
yamarko <markgrks@gmail.com>
2025-06-14 13:58:48 +0300

    Add BreakInterval model and register it in admin

--- breaks/models.py
+++ breaks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class BreakInterval(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    interval_minutes = models.PositiveIntegerField(
+        default=60,
+        validators=[MinValueValidator(5), MaxValueValidator(480)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - every {self.interval_minutes} min"
